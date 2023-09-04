8a43724d1064a736587a58c73fefbf4702018a6f
Daniel Xerxes <133733629+ReyXerxezz@users.noreply.github.com>
2023-09-03 23:52:41 -0500

    Add files via upload

---
+++ Banco/Banco/src/Clases/Cuenta_Ahorros.java
@@ -0,0 +1,36 @@
+/*
+ * 
+ * 
+ */
+package Clases;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Cuenta_Ahorros extends Cuenta{
+
+    public Cuenta_Ahorros(String Pin, float Saldo, String numCuenta) {
+        super(null, 0, null);
+    }
+    @Override
+    public void setPin(String Pin) {
+        super.setPin(Pin);
+    }
+
+    @Override
+    public void setSaldo(float Saldo) {
+        super.setSaldo(Saldo);
+    }
+
+    @Override
+    public String getPin() {
+        return super.getPin();
+    }
+
+    @Override
+    public float getSaldo() {
+        return super.getSaldo();
+    }
+    
+}
