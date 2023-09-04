1891c2fed5ce003643d85d9f8bd28730b91c1263
Santijar05 <142135343+Santijar05@users.noreply.github.com>
2023-09-03 20:08:25 -0500

    Update Usuario.java
    Creación de metodos y atributos

--- Banco/src/Clases/Usuario.java
+++ Banco/src/Clases/Usuario.java
@@ -9,5 +9,64 @@ package Clases;
  * @author Daniel
  */
 public class Usuario {
+   private String Nombre;
+    private String id;
+    private Cuenta C[];
+
+    public Usuario() {
+    }
+
+    public Usuario(String Nombre, String id, Cuenta[] C) {
+        this.Nombre = Nombre;
+        this.id = id;
+        this.C = C;
+    }
     
+    public void Crearcuenta(String n, float saldo){
+        Random random = new Random();
+        int numC = random.nextInt(900000000) + 100000000;
+        Cuenta c = new Cuenta(n,saldo, String.valueOf(numC));
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the C
+     */
+    public Cuenta[] getC() {
+        return C;
+    }
+
+    /**
+     * @param C the C to set
+     */
+    public void setC(Cuenta[] C) {
+        this.C = C;
+    }  
 }
